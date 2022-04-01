62d8b59051d314b3c63f755fcda0f312cb48ae68
STARRR777 <arstarovatov@mail.ru>
2022-04-01 12:03:26 +0700

    start length

--- app/src/test/java/com/example/selfproject/ExampleUnitTest.java
+++ app/src/test/java/com/example/selfproject/ExampleUnitTest.java
@@ -10,8 +10,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    int countPairs(int[] array) {
+        int s = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0;  j < array.length; j++) {
+
+                s++;
+                break;
+            }
+        }
+        return s;
+    }
+
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+        assertEquals(1, countPairs(new int[]{-1, 1, 0, 1}));
+
     }
 }
